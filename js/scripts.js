f5d0ccd19332c4eb221cd6bb08e13dd5d8c6d7ff
sharonhalliday <hallidaysharon5@gmail.com>
2023-12-05 19:47:21 -0800

    second commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,34 @@
 $(document).ready(function() {
     $("button#hello").click(function() {
-    $("ul").prepend("<li>Bark bark!</li>");
+    $("ul").append("<li>Bark Bark!</li>");
     });
 
         
     $("button#stop").click(function() {
-    $("ul").prepend("<li>Meow!</li>");
+    $("ul").append("<li>Meow!</li>");
     });
     });
 
 $(document).ready(function() {
     $("button#hello").click(function() {
-      $("ul#user").prepend("<li>Meow</li>");
-      $("ul#webpage").prepend("<li>Bark Bark!</li>");
+      $("ul#user").append("<li>Bark Bark!</li>");
+      $("ul#webpage").append("<li>Meow!</li>");
       $('li').css('background-color', 'green');
     });
   
   
     $("button#stop").click(function() {
-      $("ul#user").prepend("<li>Bark bark!</li>");
-      $("ul#webpage").prepend("<li>Meow.</li>");
+      $("ul#user").append("<li>Meow!</li>");
+      $("ul#webpage").append("<li>Bark Bark!</li>");
       $('li').css('background-color', 'green');
     });
+
+    // $("ul#user").children("li").first().click(function() {
+    // $(this).remove();
+    // });
+    // $("ul#webpage").children("li").first().click(function() {
+    // $(this).remove();
+    // });
   });
    
     
